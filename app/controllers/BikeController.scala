92d76b2fe8ca2f19c1f38482c91b6887ffa8970d
Emiliano Menéndez <emystein@gmail.com>
2020-06-21 13:35:10 -0300

    BikeController create and retrieve to and from repository respectively.

--- app/controllers/BikeController.scala
+++ app/controllers/BikeController.scala
@@ -1,12 +1,22 @@
 package controllers
 
-import ar.com.flow.bikerental.model.BikeRepository
+import ar.com.flow.bikerental.model.{Bike, BikeRepository}
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{AnyContent, BaseController, ControllerComponents, Request}
+import play.api.libs.json._
 
 @Singleton
 class BikeController @Inject()(val controllerComponents: ControllerComponents, val bikeRepository: BikeRepository) extends BaseController {
   def create(serialNumber: String) = Action { implicit request: Request[AnyContent] =>
-    Ok("Created bike with serial number: " + serialNumber)
+    bikeRepository.save(Bike(serialNumber))
+    Created(serialNumber)
+  }
+
+  def retrieve(serialNumber: String) = Action { implicit request: Request[AnyContent] =>
+    implicit val bikeWrites = Json.writes[Bike]
+
+    bikeRepository.getBySerialNumber(serialNumber)
+      .map(bike => Ok(Json.toJson(bike)))
+      .getOrElse(NotFound(serialNumber))
   }
 }
